28d758727aa9b9a89f0e66a2d2f92366dd0767c0
Ponang25 <102429061+Ponang25@users.noreply.github.com>
2022-05-16 05:20:39 +0800

    Update channel_post.py

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -51,7 +51,7 @@ async def channel_post(client: Client, message: Message):
     )
 
     await reply_text.edit(
-        f"<b>Link Sharing File Berhasil Di Buat :</b>\n\n{link}",
+        f"<b>Link Nonton :</b>\n\n{link}",
         reply_markup=reply_markup,
         disable_web_page_preview=True,
     )
