ad31178c4c04de9dd163f8493651f2191fca5c15
Taufik <98956280+Tpk25@users.noreply.github.com>
2022-05-30 15:25:20 +0800

    Update button.py

--- plugins/button.py
+++ plugins/button.py
@@ -2,12 +2,12 @@
 # FROM File-Sharing-Man <https://github.com/mrismanaziz/File-Sharing-Man/>
 # t.me/SharingUserbot & t.me/Lunatic0de
 
-from config import FORCE_SUB_CHANNEL, FORCE_SUB_GROUP
+from config import FORCE_SUB_CHANNEL1, FORCE_SUB_CHANNEL2, FORCE_SUB_CHANNEL3
 from pyrogram.types import InlineKeyboardButton
 
 
 def start_button(client):
-    if not FORCE_SUB_CHANNEL and not FORCE_SUB_GROUP:
+    if not FORCE_SUB_CHANNEL1 and not FORCE_SUB_CHANNEL2:
         buttons = [
             [
                 InlineKeyboardButton(text="• Versi •", callback_data="about"),
@@ -15,7 +15,7 @@ def start_button(client):
             ],
         ]
         return buttons
-    if not FORCE_SUB_CHANNEL and FORCE_SUB_GROUP:
+    if not FORCE_SUB_CHANNEL1 and FORCE_SUB_CHANNEL2:
         buttons = [
             [
                 InlineKeyboardButton(text="Join Asupan 2", url=client.invitelink2),
@@ -26,7 +26,7 @@ def start_button(client):
             ],
         ]
         return buttons
-    if FORCE_SUB_CHANNEL and not FORCE_SUB_GROUP:
+    if FORCE_SUB_CHANNEL1 and not FORCE_SUB_CHANNEL2:
         buttons = [
             [
                 InlineKeyboardButton(text="Join Asupan 1", url=client.invitelink),
@@ -37,18 +37,7 @@ def start_button(client):
             ],
         ]
         return buttons
-    if FORCE_SUB_CHANNEL and not FORCE_SUB_GROUP:
-        buttons = [
-            [
-                InlineKeyboardButton(text="Join Asupan 3", url=client.invitelink3),
-            ],
-            [
-                InlineKeyboardButton(text="• Versi •", callback_data="about"),
-                InlineKeyboardButton(text="• Tutup •", callback_data="close"),
-            ],
-        ]
-        return buttons
-    if FORCE_SUB_CHANNEL and not FORCE_SUB_GROUP:
+    if FORCE_SUB_CHANNEL1 and not FORCE_SUB_CHANNEL3:
         buttons = [
             [
                 InlineKeyboardButton(text="Join Asupan 3", url=client.invitelink3),
@@ -60,7 +49,7 @@ def start_button(client):
         ]
         return buttons
 
-    if FORCE_SUB_CHANNEL and FORCE_SUB_GROUP:
+    if FORCE_SUB_CHANNEL1 and FORCE_SUB_CHANNEL3:
         buttons = [
             [
                 InlineKeyboardButton(text="• Versi •", callback_data="about"),
@@ -76,7 +65,7 @@ def start_button(client):
 
 
 def fsub_button(client, message):
-    if not FORCE_SUB_CHANNEL and FORCE_SUB_GROUP:
+    if not FORCE_SUB_CHANNEL1 and FORCE_SUB_CHANNEL2:
         buttons = [
             [
                 InlineKeyboardButton(text="Join Asupan 2", url=client.invitelink2),
@@ -94,7 +83,7 @@ def fsub_button(client, message):
         except IndexError:
             pass
         return buttons
-    if FORCE_SUB_CHANNEL and not FORCE_SUB_GROUP:
+    if FORCE_SUB_CHANNEL1 and not FORCE_SUB_CHANNEL3:
         buttons = [
             [
                 InlineKeyboardButton(text="Join Asupan 1", url=client.invitelink),
@@ -112,7 +101,7 @@ def fsub_button(client, message):
         except IndexError:
             pass
         return button
-    if FORCE_SUB_CHANNEL and not FORCE_SUB_GROUP:
+    if FORCE_SUB_CHANNEL2 and not FORCE_SUB_CHANNEL3:
         buttons = [
             [
                 InlineKeyboardButton(text="Join Asupan 3", url=client.invitelink3),
@@ -130,7 +119,7 @@ def fsub_button(client, message):
         except IndexError:
             pass
         return buttons
-    if FORCE_SUB_CHANNEL and FORCE_SUB_GROUP:
+    if FORCE_SUB_CHANNEL1 and FORCE_SUB_CHANNEL3:
         buttons = [
             [
                 InlineKeyboardButton(text="Join Asupan 1", url=client.invitelink),
